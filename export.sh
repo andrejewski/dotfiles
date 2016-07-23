5d6d72889ff8a1c26d3903c3c20b41f0ef28f0f5
Chris Andrejewski <christopher.andrejewski@gmail.com>
2016-07-23 08:33:20 -0400

    using sync-settings for atom instead

--- export.sh
+++ export.sh
@@ -11,10 +11,5 @@ mkdir -p ./.vim/colors
 cp ~/.vim/colors/yule.vim ./.vim/colors/yule.vim
 cp ~/.vim/colors/256-grayvim.vim ./.vim/colors/256-grayvim.vim
 
-# atom
-mkdir -p ./.atom
-cp ~/.atom/keymap.cson ./.atom/
-apm list --installed --bare > ./.atom/packages.txt
-
 # git
 cp ~/.gitconfig ./.gitconfig
--- import.sh
+++ import.sh
@@ -9,11 +9,6 @@ source ~/.bashrc
 # vim
 cp ./.vimrc ~/.vimrc
 
-# atom
-mkdir -p ~/.atom
-cp -R ./.atom ~/.atom
-apm install --packages-file ./.atom/packages.txt
-
 mkdir -p ~/.vim/colors
 cp ./.vim/colors/yule.vim ~/.vim/colors/yule.vim
 cp ./.vim/colors/256-grayvim.vim ~/.vim/colors/256-grayvim.vim
@@ -21,10 +16,6 @@ cp ./.vim/colors/256-grayvim.vim ~/.vim/colors/256-grayvim.vim
 mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
-
-cd ~/.vim/bundle/YouCompleteMe
-./install.py --clang-completer --tern-completer
-cd -
 
 # git
 cp ./.gitconfig ~/.gitconfig
